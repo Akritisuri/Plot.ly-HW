672d7b82c238e8634cb30499244adfe80fb0344e
Akriti Suri <akritisuri96@gmail.com>
2020-12-06 22:17:24 -0800

    two

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,9 @@
 function buildMetadata(sample) { 
     d3.json("samples.json").then((data) => {
         var metadata = data.metadata;
+        var resultarray = metadata.filter(sampleobject => sampleobject.id == sample); 
+        var result = resultarray[0] 
+        var PANEL = d3.select("#sample-metadata");
         
     }
 }
