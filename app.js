f12c35a5e25ff250b7e43a77d918e50fea6fd570
Akriti Suri <akritisuri96@gmail.com>
2020-12-08 21:36:26 -0800

    four

--- app.js
+++ app.js
@@ -19,40 +19,72 @@ console.log(d3.json("samples.json"));
             var location = meta_data[0].location
             var wfreq = meta_data[0].wfreq 
 
-            var otu_ids = samples[0].otu_ids 
-            var otu_labels = samples[0]
+            var otu_ids = sample_data[0].otu_ids 
+            var otu_labels = sample_data[0].otu_labels 
+            var sample_values = sample_data[0].sample_values 
+            var sample_values_split = sample_values.slice(0,10).reverse() 
+            for (var i=0; i < otu_ids.length; i++) { 
+                otu_ids[i] = "OTU " + otu_ids[i]; 
+
+            var traceA = {
+            type: "bar", 
+            x: sample_values_split, 
+            y: otu_ids, 
+            orientation: "h", 
+            text: otu_labels
+
+            }
+
+            var data [traceA]
+
+            var layout = {
+                title: `${changed_id} Belly Button Biodiversity Data` 
+            } 
+
+            var CHART = d3.selectALL("#bar").node();
+
+            Plotly.newPlot("bar", data, layout) 
+
+            var traceB = { 
+            x: otu_ids, 
+            y: samples_values_split,
+            text: otu_labels, 
+            mode: 'markers',
+            marker: { 
+                size: (sample_values_split_), 
+                sizeref: 3
+            }
+
+        }
+
+            var layout = {
+                title: `Belly Button Biodiversity Data ${changed_id}`
+            }
+
+            Plotly.newPlot("bubble", data, layout) 
+
+            var traceC = {
+                domain: { x: [0, 1], y: [0, 1]}, 
+                value: wfreq,
+                type: "indicator", 
+                mode: "gauge+number", 
+                title: { text: `Wash Frequency ${changed_id`}, 
+                gauge: { 
+                    axis: { range: [null, 9] },
+                }
+            }
+
+            var data = [traceC]
+
+            var layout = {
+                width: 500, 
+                height: 500,
+        }
+
+        Plotly.newPlot(gauge, data, layout);
 
 
+        } 
+        }) 
+    }
     
-            var ids = sample_data[0].otu_ids
-            console.log(ids)
-            var SampleVal = sample_data[0].sample_values.slice(0,10).reverse();
-            console.log(SampleVal)
-            var data_labels = sample_data[0].otu_labels.slice(0,10); 
-            console.log(data_labels)
-            var OTU_TopTen = (sample_data[0].otu_ids.slice(0,10)).reverse();
-            console.log(OTU_TopTen)
-            var OTU_id = OTU_TopTen.map(d => "OTU" + d);
-            var labels = sample_data[0].otu_labels.slice(0,10); 
-
-            console.log(`OTU IDS: ${OTU_id}`) 
-            console.log(`OTU_labels: ${labels}`)
-
-            var structure = { 
-                    x: sampleValues,
-                    y: OTU_id, 
-                    text: data_labels,
-                    marker: {
-                    color: 'blue'},
-                    type: "bar",
-                    orientation: "h",
-                    };
-
-            var data = [structure]
-
-            Plotly.newPlot("Bar",data);
-            })}
-
-            gp();
-
-  
